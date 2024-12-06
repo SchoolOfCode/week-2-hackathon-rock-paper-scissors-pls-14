5f6aca80875aa4f2491e4b01b6d50b706da10ec2
Louis <178508612+L-Brookling@users.noreply.github.com>
2024-12-06 09:52:28 +0000

    Complete MVP 5

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
+
+let playAgain = true
+
+while (playAgain == true) {
 // Player choice
-let playerMove = (prompt("What move would you like?")).toLowerCase();       //Bug #3 - Had player lose if choice was input with capital letter.
+    let playerMove = (prompt("What move would you like?")).toLowerCase();   //Bug #3 - Had player lose if choice was input with capital letter.
 // Function to randomly generate Computer's choice
-let computerMove = () => {
+    let computerMove = () => {
     
-    let randomNumber = Math.floor(Math.random() * 3)                           // Bug #2 - Math.floor(Math.random() * 2       - should be * 3.
+        let randomNumber = Math.floor(Math.random() * 3)                           // Bug #2 - Math.floor(Math.random() * 2       - should be * 3.
     
-    if (randomNumber == 0) {
-        return "rock";
+        if (randomNumber == 0) {
+            return "rock";
 
-    } else if (randomNumber == 1) {
-        return "scissors";
+        } else if (randomNumber == 1) {
+            return "scissors";
 
-    } else {
-        return "paper";
-    }
-};
+        } else {
+            return "paper";
+        }
+    };
+ 
 // Function to decide who the winner is
-const getWinner = (playerMove, computerMove) => {
+    const getWinner = (playerMove, computerMove) => {
     
-    if (playerMove === computerMove) {                                            //Bug #1 - (playerMove = computerMove) should've been "===".
-        return 0; 
+        if (playerMove === computerMove) {                                            //Bug #1 - (playerMove = computerMove) should've been "===".
+            return 0; 
     
-    } else if (playerMove === "rock" && computerMove === "scissors") {
-        return 1;
+        } else if (playerMove === "rock" && computerMove === "scissors") {
+            return 1;
     
-    } else if (playerMove === "paper" && computerMove === "rock") {
-        return 1;
+        } else if (playerMove === "paper" && computerMove === "rock") {
+            return 1;
         
-    } else if (playerMove === "scissors" && computerMove === "paper") {
-        return 1;
+        } else if (playerMove === "scissors" && computerMove === "paper") {
+            return 1;
     
-    } else {
-        return -1;
-    }
-};
+        } else {
+            return -1;
+        }
+    };
+
 // Variable to show game results
-let result = getWinner(playerMove, computerMove());
-console.log(result)
-alert(`The computer chose ${computerMove()}!`)
+    let result = getWinner(playerMove, computerMove());
+    console.log(result)
+    alert(`The computer chose ${computerMove()}!`)
+
+// Ask if player wants to continue    
+    playAgain = confirm("Do you want to play again?");
+
+    if (!playAgain) {
+    alert(`Game over!`);
+    }
+
+};                                                                                  
+
+
+
+
+
+
+
+
+
+/*
+switch (randomNumber) {
+    case (randomNumber == 0):
+        return "rock";
+        break;
+    case (randomNumber == 1):
+        return "paper";
+        break;
+    default:
+        return "scissors";
+    };
+*/
